28d86df99f66ef16a002c5af0317aa143b5555e9
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-02-09 08:46:36 +0530

    send the image buffer directly

--- api/index.js
+++ api/index.js
@@ -8,15 +8,21 @@ app.use(json());
 
 app.use(urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.get("/", async (req, res) => {
+  console.log(
+    path.join(__dirname, "../public/67s8KFihePTg3bgBBnjEoj1n.ttf.fnt")
+  );
+
   if (!req.body?.title) {
     res.type("jpg");
     res.sendFile(path.join(__dirname, "../public/base.png"));
     return;
   }
 
-  res.type("jpg");
-  res.attachment(path.join(__dirname, "../public/output.png"));
+  res.setHeader("Content-Type", "image/png");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   const font = await jimp.loadFont(
     path.join(__dirname, "../public/67s8KFihePTg3bgBBnjEoj1n.ttf.fnt")
   );
@@ -29,7 +35,7 @@ app.get("/", async (req, res) => {
 
       const date = new Date();
 
-      await lenna
+      const image = await lenna
         .print(font, 302, 390, req.body?.title, 1621)
         .print(
           font,
@@ -38,9 +44,23 @@ app.get("/", async (req, res) => {
           `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
           1621
         )
-        .quality(100)
-        .writeAsync(path.join(__dirname, "../public/output.png")); // save
-      res.download(path.join(__dirname, "../public/output.png"));
+        .quality(100);
+
+      image.getBase64(jimp.AUTO, (err, src) => {
+        if (err) throw err;
+        const base64Img = src.split(",")[1];
+
+        const buffer = new Buffer.from(
+          base64Img.replace(/^data:image\/\w+;base64,/, ""),
+          "base64"
+        );
+
+        res.writeHead(200, {
+          "Content-Type": "image/png",
+        });
+
+        res.download(buffer, "image.png");
+      });
     }
   );
 });
