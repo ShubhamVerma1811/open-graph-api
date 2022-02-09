729772e4f99e8a2554c2db726b539645c07b3e72
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-02-09 07:57:57 +0530

    vercel setup

---
+++ api/index.js
@@ -0,0 +1,47 @@
+import express, { json, urlencoded } from "express";
+import jimp from "jimp";
+import path from "path";
+
+const app = express();
+
+app.use(json());
+
+app.use(urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  if (!req.body?.title) {
+    res.status(200).send("Missing title");
+    return;
+  }
+
+  res.type("jpg");
+  res.attachment(path.join(__dirname, "../public/output.png"));
+  const font = await jimp.loadFont(
+    path.join(__dirname, "../public/67s8KFihePTg3bgBBnjEoj1n.ttf.fnt")
+  );
+
+  jimp.read(
+    path.join(__dirname, "../public/base.png"),
+
+    async (err, lenna) => {
+      if (err) throw err;
+
+      const date = new Date();
+
+      await lenna
+        .print(font, 302, 390, req.body?.title, 1621)
+        .print(
+          font,
+          302,
+          744,
+          `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+          1621
+        )
+        .quality(100)
+        .writeAsync(path.join(__dirname, "../public/output.png")); // save
+      res.download(path.join(__dirname, "../public/output.png"));
+    }
+  );
+});
+
+module.exports = app;
